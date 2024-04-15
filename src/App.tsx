1cd758d0cbb7268954802900d83f83b0fee4938c
Amadeusz Lisiecki <amadeusz.lisiecki@onet.pl>
2024-04-15 10:43:04 +0200

    Add color theme autodetector

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { HashRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 import './App.css';
 import { TagsContent } from './components/TagsContent';
@@ -7,10 +10,24 @@ import { TagsContent } from './components/TagsContent';
 const queryClient = new QueryClient();
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
     <HashRouter>
       <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
         <TagsContent />
+      </ThemeProvider>
       </QueryClientProvider>
     </HashRouter>
   );
